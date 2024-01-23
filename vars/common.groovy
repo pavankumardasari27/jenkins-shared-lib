3b783c8cb6faa0156405b2876e23661b98b7fdff
pavankumardasari27 <153920823+pavankumardasari27@users.noreply.github.com>
2024-01-23 16:02:20 +0530

    Update common.groovy

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,22 +1,24 @@
+// vars/common.groovy 
+
 def checkoutGitRepo(String url, String branch = 'main') {
   git url: url, branch: branch, credentialsId: 'git-credentials'
 }
 
 def setPermissions(String path) {
   sh "sudo chown -R www-data:www-data ${path}"
-  sh "sudo chmod -R 755 ${path}" 
+  sh "sudo chmod -R 755 ${path}"
   sh "sudo chmod -R 644 ${path}/*"
 }
 
 def installDependencies(String path) {
-  sh "cd ${path} && composer install"
+  sh "cd ${path} && composer install" 
   sh "cd ${path} && php artisan config:cache"
-  sh "cd ${path} && php artisan migrate"  
-}
+  sh "cd ${path} && php artisan migrate"
+} 
 
 def runCodeQuality(String path) {
   sh "cd ${path} && composer require laravel/pint --dev"
-  sh "cd ${path} && ./vendor/bin/pint --test"
+  sh "cd ${path} && ./vendor/bin/pint --test" 
 }
 
 def startApp(String path, String host, String port) {
