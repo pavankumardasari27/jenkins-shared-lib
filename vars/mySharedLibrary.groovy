e43ee7a4a07d75778c4ff4693c39fb2c29197e0a
pavankumardasari27 <153920823+pavankumardasari27@users.noreply.github.com>
2024-01-24 15:44:02 +0530

    Update mySharedLibrary.groovy

--- vars/mySharedLibrary.groovy
+++ vars/mySharedLibrary.groovy
@@ -49,10 +49,10 @@ def codeQualityTesting() {
 def runLaravelApp() {
   dir("${WORKSPACE}") {
     try {
-      sh 'php artisan --version'
+      sh 'php artisan --version'  
       sh 'php artisan serve --host=0.0.0.0 --port=8000 > laravel.log 2>&1 &'
-      sh 'sleep 10' // wait for app to start
-      sh 'curl http://localhost:8000' // test if running
+      sh 'sleep 10' 
+      sh 'curl http://localhost:8000' 
     } catch (Exception e) {
       echo "Failed to start Laravel app: ${e}"
       throw e
