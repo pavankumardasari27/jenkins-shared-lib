fd496359b36c236433d52729ce3b965415666c68
pavankumardasari27 <153920823+pavankumardasari27@users.noreply.github.com>
2024-01-24 07:39:04 +0530

    Create mySharedLibrary.groovy

---
+++ vars/mySharedLibrary.groovy
@@ -0,0 +1,57 @@
+def checkout(String credentialsId, String gitRepo) {
+    git credentialsId: credentialsId, url: gitRepo
+}
+
+def sshToServer(String pemKey, String serverIp) {
+    sshagent([pemKey]) {
+        sh """
+        ssh -o StrictHostKeyChecking=no -l ubuntu ${serverIp} "ping -c 4 ${serverIp}"
+        """
+    }
+}
+
+def givePermissions() {
+    sh """
+    sudo chown www-data:www-data -R *
+    sudo find . -type d -exec chmod 755 {} \\;
+    sudo find . -type f -exec chmod 644 {} \\;
+    """
+}
+
+def composerInstallAndSetup() {
+    sh """
+    composer install
+    php artisan config:cache
+    php artisan migrate
+    php artisan db:seed
+    """
+}
+
+def codeQualityTesting() {
+    sh """
+    composer require laravel/pint --dev
+    ./vendor/bin/pint --test
+    ./vendor/bin/pint -v
+    composer require nunomaduro/phpinsights --dev
+    ./vendor/bin/phpinsights
+    php artisan insights --fix
+    """
+}
+
+def runApplication() {
+    sh """
+    php artisan serve --host=0.0.0.0 --port=8000 &
+    sleep 5
+    curl -I http://localhost:8000
+    """
+}
+
+def setupNginx(String serverIp) {
+    sh """
+    ssh -o StrictHostKeyChecking=no -l ubuntu ${serverIp} << EOF
+    sudo cp /path/to/your/nginx.conf /etc/nginx/sites-available/
+    sudo ln -s /etc/nginx/sites-available/nginx.conf /etc/nginx/sites-enabled/
+    sudo service nginx restart
+    EOF
+    """
+}
