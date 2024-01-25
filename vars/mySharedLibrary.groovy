c8f105a6da2eb4e4bf1809ea40990b457b06f1eb
pavankumardasari27 <153920823+pavankumardasari27@users.noreply.github.com>
2024-01-25 17:50:33 +0530

    Update mySharedLibrary.groovy

--- vars/mySharedLibrary.groovy
+++ vars/mySharedLibrary.groovy
@@ -31,6 +31,7 @@ def composerInstallAndSetup() {
     sudo chmod -R 755 .
     composer update
     sudo apt-get install php-xml
+    sudo apt-get install php-pdo php-mysql
     sudo chown jenkins:jenkins composer.lock
     sudo chmod 664 composer.lock
     sudo chmod -R 777 storage
